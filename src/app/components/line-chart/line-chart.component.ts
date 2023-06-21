50a8c54ee82da2ae6e6c7a15e8da9f4bcf27a114
manuallende <manuel.allende2@gmail.com>
2023-06-20 23:04:34 -0300

    Avances Front

--- src/app/components/line-chart/line-chart.component.ts
+++ src/app/components/line-chart/line-chart.component.ts
@@ -1,66 +1,109 @@
-import { Component, ViewChild, Input, OnInit, AfterViewInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
 import { multi } from './data';
-import {Color, LegendPosition, ScaleType} from "@swimlane/ngx-charts";
-import {single} from "rxjs/operators";
-import { DataService } from 'src/app/services/data.service';
-
 
 @Component({
   selector: 'app-line-chart',
-  templateUrl: './line-chart.component.html',
+  template: `
+    <ngx-charts-line-chart
+      [view]="view"
+      [scheme]="colorScheme"
+      [results]="lineChartData"
+      [legend]="showLegend"
+      [xAxis]="showXAxis"
+      [yAxis]="showYAxis"
+      [showXAxisLabel]="showXAxisLabel"
+      [showYAxisLabel]="showYAxisLabel"
+      [xAxisLabel]="xAxisLabel"
+      [yAxisLabel]="yAxisLabel"
+    ></ngx-charts-line-chart>
+  `,
   styleUrls: ['./line-chart.component.scss']
 })
+export class LineChartComponent {
+  @Input() lineChartData: any[] = multi;
+  @Input() showLegend: boolean = true;
+  @Input() showXAxis: boolean = true;
+  @Input() showYAxis: boolean = true;
+  @Input() showXAxisLabel: boolean = false;
+  @Input() showYAxisLabel: boolean = false;
+  @Input() xAxisLabel: string = '';
+  @Input() yAxisLabel: string = '';
 
-export class LineChartComponent implements OnInit, AfterViewInit{
-  
-  @Input() view: [number, number] = [700, 400]; // width, height
-  legendPosition = LegendPosition.Below;
+  view: [number, number] = [700, 400]; // Ancho, alto
   colorScheme: Color = {
     name: 'myScheme',
     selectable: true,
     group: ScaleType.Ordinal,
-    domain: [],
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
-  @Input() lineChartData: any[] = [];
-  @Input() showLegend: boolean = true;
-  @Input() showLabels: boolean = true;
-  @Input() showXAxisLabel: boolean = false;
-  @Input() showYAxisLabel: boolean = false;
-  @Input() yAxisLabel: string = '';
-  @Input() xAxisLabel: string = '';
 
-  // options
-  // animations: boolean = true;
-  // xAxis: boolean = true;
-  // yAxis: boolean = true;
-  // timeline: boolean = true;
+}
 
 
-  constructor(private dataService: DataService) {
-    // Object.assign(this, { single });
-    Object.assign(this, { multi });
-  }
+// import { Component, ViewChild, Input, OnInit, AfterViewInit } from '@angular/core';
+// import { multi } from './data';
+// import {Color, LegendPosition, ScaleType} from "@swimlane/ngx-charts";
+// import {single} from "rxjs/operators";
+// import { DataService } from 'src/app/services/data.service';
 
-  ngOnInit(): void {
-    // this.initData();
-  }
 
-  ngAfterViewInit(): void {
-    let length = 0;
-    if (this.lineChartData.length > 0) length = this.lineChartData.length;
+// @Component({
+//   selector: 'app-line-chart',
+//   templateUrl: './line-chart.component.html',
+//   styleUrls: ['./line-chart.component.scss']
+// })
 
-    for (let i = 0; i < length; i++) {
-      this.colorScheme.domain.push(this.getRandomColor());
-    }
-  }
+// export class LineChartComponent implements OnInit, AfterViewInit{
+  
+//   @Input() view: [number, number] = [700, 400]; // width, height
+//   legendPosition = LegendPosition.Below;
+//   colorScheme: Color = {
+//     name: 'myScheme',
+//     selectable: true,
+//     group: ScaleType.Ordinal,
+//     domain: [],
+//   };
+//   @Input() lineChartData: any[] = [];
+//   @Input() showLegend: boolean = true;
+//   @Input() showLabels: boolean = true;
+//   @Input() showXAxisLabel: boolean = false;
+//   @Input() showYAxisLabel: boolean = false;
+//   @Input() yAxisLabel: string = '';
+//   @Input() xAxisLabel: string = '';
 
-  getRandomColor() {
-    let color = Math.floor(0x1000000 * Math.random()).toString(16);
-    return '#' + ('000000' + color).slice(-6);
-  }
+//   // options
+//   // animations: boolean = true;
+//   // xAxis: boolean = true;
+//   // yAxis: boolean = true;
+//   // timeline: boolean = true;
+
+
+//   constructor(private dataService: DataService) {
+//     // Object.assign(this, { single });
+//     Object.assign(this, { multi });
+//   }
+
+//   ngOnInit(): void {
+//     // this.initData();
+//   }
+
+//   ngAfterViewInit(): void {
+//     let length = 0;
+//     if (this.lineChartData.length > 0) length = this.lineChartData.length;
+
+//     for (let i = 0; i < length; i++) {
+//       this.colorScheme.domain.push(this.getRandomColor());
+//     }
+//   }
+
+//   getRandomColor() {
+//     let color = Math.floor(0x1000000 * Math.random()).toString(16);
+//     return '#' + ('000000' + color).slice(-6);
+//   }
 
   // private async initData(){
   //    this.dataService.getData().subscribe((response) => )
   // }
 
-}
+// }
